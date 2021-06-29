c005408b6daa433cbf827d7fe66b24dd7ecdc716
Rizal-04 <rizalxfriends655@gmail.com>
2021-06-29 21:36:57 +0700

    clear

---
+++ kulineran-RizalL/src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="mt-5 mb-5">
+    <hr />
+    <div class="container">
+      <div class="row">
+        <div class="col text-center">25-Mei-2021 Copyright RizalL-Muhaimin</div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style></style>
